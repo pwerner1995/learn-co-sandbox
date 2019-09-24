48d550cdeedbf209d5036820e62d9121475976c5
Peter Werner <pcw1995@yahoo.com>
2019-09-24 16:35:35 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,4 @@
+def greeting
+  puts "What's up?"
+end
+greeting
---
+++ if-end.rb
@@ -0,0 +1,8 @@
+chance_of_rain = 100
+if chance_of_rain <= 0.25
+  puts "Put on sunscreen"
+elsif (chance_of_rain >0.25 && chance_of_rain<0.75)
+  puts "Pack an umbrella!"
+else  
+  puts "stay at home"
+end
